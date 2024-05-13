5741f39fc476d2d34c51fb2676f9328f2c9f80a8
Marcos Kennedy <marcosnogueira997@gmail.com>
2024-05-13 11:36:30 -0300

    exclusão método OPTIONS

--- biblioteca/src/main/java/com/projetos/biblioteca/infra/cors/CorsConfig.java
+++ biblioteca/src/main/java/com/projetos/biblioteca/infra/cors/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*") // Permitindo solicitações de qualquer origem
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+            .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
             .allowedHeaders("Content-Type", "Authorization"); // Cabeçalhos permitidos
     }
 }
