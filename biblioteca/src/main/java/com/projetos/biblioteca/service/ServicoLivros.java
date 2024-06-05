042f35c651e66d49df3ee30981361a95b89246a1
blackcoffee6681 <100376218+marcos-kennedy@users.noreply.github.com>
2024-06-05 15:23:21 -0300

    reformulação da lógica de devolução de livros

--- biblioteca/src/main/java/com/projetos/biblioteca/service/ServicoLivros.java
+++ biblioteca/src/main/java/com/projetos/biblioteca/service/ServicoLivros.java
@@ -1,12 +1,17 @@
 package com.projetos.biblioteca.service;
 
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.projetos.biblioteca.models.Aluno;
 import com.projetos.biblioteca.models.Livro;
+import com.projetos.biblioteca.models.Registro;
+import com.projetos.biblioteca.repositorys.AlunoRepository;
 import com.projetos.biblioteca.repositorys.LivroRepository;
 import com.projetos.biblioteca.repositorys.RegistroRepository;
 
@@ -16,6 +21,8 @@ public class ServicoLivros {
     private LivroRepository livroRepository;
     @Autowired
     private RegistroRepository registroRepository;
+    @Autowired
+    private AlunoRepository alunoRepository;
     private Mensagem mensagem = new Mensagem();
 
     public ResponseEntity<?> cadastrar(Livro livro) {
@@ -29,8 +36,20 @@ public class ServicoLivros {
             return new ResponseEntity<>(livroRepository.save(livro), HttpStatus.OK);
         }
     }
-    public ResponseEntity<?> devolver(String nomeLivro){
-        Livro livroRetornado = livroRepository.findByNomeLivro(nomeLivro);
+    public ResponseEntity<?> devolver(Integer idRegistro){
+        Optional<Registro> registroOptional = registroRepository.findById(idRegistro);
+        Registro registroRetornado = null;
+        Livro livroRetornado = null;
+        Aluno alunoRetornado = null;
+
+        if (registroOptional.isPresent()) {
+            registroRetornado = registroOptional.get();
+            livroRetornado = registroRetornado.getLivro();
+            alunoRetornado = registroRetornado.getAluno();
+        } else {
+            mensagem.setMensagem("Erro! Registro de empréstimo não encontrado!");
+            return new ResponseEntity<>(mensagem, HttpStatus.BAD_REQUEST);
+        }
         if(livroRetornado == null){
             mensagem.setMensagem("Erro! Livro não encontrado.");
             return new ResponseEntity<>(mensagem, HttpStatus.BAD_REQUEST);
@@ -38,6 +57,8 @@ public class ServicoLivros {
             Integer numeroExemplaresAtualizado = livroRetornado.getNumeroExemplares() + 1;
             livroRetornado.setNumeroExemplares(numeroExemplaresAtualizado);
             livroRepository.save(livroRetornado);
+            registroRepository.delete(registroRetornado);
+            alunoRepository.delete(alunoRetornado);
             mensagem.setMensagem("Livro devolvido com sucesso!");
             return new ResponseEntity<>(mensagem, HttpStatus.OK);
         }
